37623035be40e649381b7350b3cb2f09df1e1aac
Даниил Полянский <dm.tsiganov@gmail.com>
2022-12-08 04:37:52 +0700

    Решение

---
+++ Program.cs
@@ -0,0 +1,47 @@
+void FillNewArray(string[] tempArray, string[] oldArray)
+{
+    Console.Write("[");
+    int i = 0;
+    int j = 0;
+    {
+        for (i = 0; i < oldArray.Length; i++)
+        {
+            string s = oldArray[i];
+            if (s.Length <= 3)
+            {
+                tempArray[j] = oldArray[i];
+                
+                Console.Write($"{tempArray[j]}");
+                 if (i < tempArray.Length - 1)
+                    {
+                        Console.Write(", ");
+                    }
+                j++;
+
+            }
+        }
+    }
+Console.Write("]");
+}
+
+Console.Write("Введите размер массива: ");
+string inputNumberOfArray = Console.ReadLine();
+int numberOfArray = int.Parse(inputNumberOfArray);
+
+string[] arrayOfString = new string[numberOfArray];
+int i = 0;
+int count = 0;
+for (i = 0; i < numberOfArray; i++)
+{
+    Console.Write($"Введите {i + 1}-й элемент массива: ");
+    arrayOfString[i] = Console.ReadLine();
+    string s = arrayOfString[i];
+    if (s.Length <= 3)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Новый массив будет состоять из {count} элементов ");
+string[] newArray = new string[count];
+Console.WriteLine();    
+FillNewArray(newArray, arrayOfString);
